c1e267d82693b0abb0d97e8f6b77bb93c1242027
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-12-21 12:58:55 +1000

    Minor enhancements with 'complete' tasks and stats

--- tasks.js
+++ tasks.js
@@ -5,23 +5,32 @@ Tasks.allow({
 	remove: function(){return true;},
 });
 
-Tasks.URGENCY = ['Not urgent', 'Urgent'];
-Tasks.IMPORTANCE = ['Not importance', 'Important'];
+Tasks.URGENCY = ['', 'Urgent'];
+Tasks.IMPORTANCE = ['', 'Important'];
 
 if (Meteor.isClient) {
-  Template.AddTask.events({
-    'click #add': function (e) {
-    	console.log($('#importance').val());
-      Tasks.insert({
+	function addTask(e){
+		Tasks.insert({
       	description: $('#description').val(),
       	urgency: $('#urgency').val(),
-      	importance: $('#importance').val()
-      });
+      	importance: $('#importance').val(),
+      	complete: false
+    });
+    $('#AddTask #description').val('');
+    $('#AddTask #importance').val('');
+    $('#AddTask #urgency').val('');
+	}
+  Template.AddTask.events({
+    'click #add': addTask,
+    'keypress input#description': function(e) {
+    	if (e.which === 13) { // if pressed enter
+    	addTask(e);
+    	}
     }
   });
 
   Template.Tasks.all = function(){
-  	return Tasks.find({}, {sort: [['importance', 'desc'],['urgency','desc']]});
+  	return Tasks.find({complete: false}, {sort: [['importance', 'desc'],['urgency','desc']]});
   };
   Template.Tasks.getUrgency = function(urgencyVal){
   	return Tasks.URGENCY[urgencyVal];
@@ -32,9 +41,16 @@ if (Meteor.isClient) {
   Template.Tasks.events({
   	'click .control-done a': function(e) {
   	  taskId = $(e.target).closest('.task')[0].getAttribute('task-id');
-  	  Tasks.remove({_id: taskId});
+  	  Tasks.update({_id: taskId}, {$set:{complete: true}});
   	}
   });
+  
+  Template.TaskStats.numberIncomplete = function(){
+  	return Tasks.find({complete: false}).count();
+  };
+  Template.TaskStats.numberComplete = function(){
+  	return Tasks.find({complete: true}).count();
+  };
 }
 
 if (Meteor.isServer) {
