841ffe3cd83ce9e5ef734210661a0a31c1e8d97d
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-12-21 16:29:20 +1000

    New design, drag and drop

--- tasks.js
+++ tasks.js
@@ -4,22 +4,21 @@ Tasks.allow({
 	update: function(){return true;},
 	remove: function(){return true;},
 });
-
-Tasks.URGENCY = ['', 'Urgent'];
-Tasks.IMPORTANCE = ['', 'Important'];
-
+  
 if (Meteor.isClient) {
 	function addTask(e){
 		Tasks.insert({
-      	description: $('#description').val(),
-      	urgency: $('#urgency').val(),
-      	importance: $('#importance').val(),
-      	complete: false
-    });
-    $('#AddTask #description').val('');
-    $('#AddTask #importance').val('');
-    $('#AddTask #urgency').val('');
+		  	description: $('#description').val(),
+		  	urgency: +$('#urgency').val(),
+		  	importance: +$('#importance').val(),
+		  	complete: false
+		});
+		$('#AddTask #description').val('');
+		$('#AddTask #importance').val('');
+		$('#AddTask #urgency').val('');
 	}
+
+
   Template.AddTask.events({
     'click #add': addTask,
     'keypress input#description': function(e) {
@@ -28,20 +27,54 @@ if (Meteor.isClient) {
     	}
     }
   });
-
-  Template.Tasks.all = function(){
-  	return Tasks.find({complete: false}, {sort: [['importance', 'desc'],['urgency','desc']]});
+  
+  Template.Tasks.allUrgentAndImportant = function(){
+  	return Tasks.find({complete: false, urgency: 1, importance: 1});
   };
-  Template.Tasks.getUrgency = function(urgencyVal){
-  	return Tasks.URGENCY[urgencyVal];
+  Template.Tasks.allImportant = function(){
+  	return Tasks.find({complete: false, urgency: 0, importance: 1});
   };
-  Template.Tasks.getImportance = function(importanceVal){
-  	return Tasks.IMPORTANCE[importanceVal];
+  Template.Tasks.allUrgent = function(){
+  	return Tasks.find({complete: false, urgency: 1, importance: 0});
   };
-  Template.Tasks.events({
-  	'click .control-done a': function(e) {
-  	  taskId = $(e.target).closest('.task')[0].getAttribute('task-id');
-  	  Tasks.update({_id: taskId}, {$set:{complete: true}});
+  Template.Tasks.allOthers = function(){
+  	return Tasks.find({complete: false, urgency: 0, importance: 0});
+  };
+  
+  Template.Buckets.events({
+  	'dragover .bucket': function(e) {
+  		e.preventDefault();
+  		$(e.target).css('color', 'rgb(69, 130, 236)');
+  		$(e.target).addClass('jiggly');
+  	},
+  	'dragleave .bucket': function(e) {
+  		e.preventDefault();
+  		$(e.target).css('color', 'rgb(51, 51, 51)');
+  		$(e.target).removeClass('jiggly');
+  	},
+  	'drop .bucket': function(e) {
+  		e.preventDefault();
+  		$(e.target).css('color', 'rgb(51, 51, 51)');
+  		$(e.target).removeClass('jiggly');
+  		var taskId = e.dataTransfer.getData('text/plain');
+  		if($(e.target).hasClass('done-bucket')) {
+  			Tasks.update({_id: taskId}, {$set:{complete: true}});
+  		} else {
+  			Tasks.remove({_id: taskId});
+  		}
+  	}
+  });
+  
+  Template.Task.events({
+  	'dragstart .task': function(e) {
+  		var task = $(e.target).closest('.task');
+  		task.css('opacity', 0.4);
+  		e.dataTransfer.effectAllowed = "move";
+  		e.dataTransfer.setData("text/plain", task[0].getAttribute('task-id'));
+  	},
+  	'dragend .task': function(e) {
+  		var task = $(e.target).closest('.task');
+  		task.css('opacity', 1.0);
   	}
   });
   
